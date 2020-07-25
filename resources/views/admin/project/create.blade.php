42b8693d825b3464f9025a2e8a16bd594a96f91d
EC2 Default User <ec2-user@ip-172-31-17-112.us-east-2.compute.internal>
2020-07-25 14:46:34 +0000

    first commit

---
+++ resources/views/admin/project/create.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.admin')
+@section('title', '北の大地')
+
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 mx-auto">
+                <h2>北海道でダーツするなら</h2>
+            </div>
+        </div>
+    </div>
+@endsection
